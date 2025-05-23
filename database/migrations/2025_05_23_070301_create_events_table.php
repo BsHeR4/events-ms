71dba13b57271471fdd6aab43da64357c3c05dec
BsHeR4 <bshermahayni@gmail.com>
2025-05-23 10:38:02 +0300

    Setup events migration

--- database/migrations/2025_05_23_070301_create_events_table.php
+++ database/migrations/2025_05_23_070301_create_events_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->foreignId('event_type_id')->constrained('event_types')->cascadeOnDelete();
+            $table->foreignId('location_id')->constrained('locations')->cascadeOnDelete();
             $table->timestamps();
         });
     }

63b0d7cb19d7440847847673850cf447d64b9dac
BsHeR4 <bshermahayni@gmail.com>
2025-05-23 18:13:54 +0300

    create RegistrationRequest

---
+++ app/Http/Requests/RegistrationRequest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\EmailRule;
+use App\Rules\PasswordRule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class RegistrationRequest extends FormRequest
+{
+
+    protected $stopOnFirstFailure = true;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'is_admin' => 'boolean',
+            'email' => [
+                'required',
+                'string',
+                'max:255',
+                new EmailRule,
+                'unique:users,email',
+            ],
+            'password' => [
+                'required',
+                'string',
+                new PasswordRule(),
+                'between:8,128',
+                'confirmed',
+            ],
+        ];
+    }
+
+    /**
+     * Handle a passed validation attempt.
+     */
+    protected function passedValidation(): void
+    {
+        $this->replace(['is_admin' => false]);
+    }
+
+    public function attributes()
+    {
+        return [
+            'first_name' => 'First Name',
+            'last_name' => 'Last Name',
+            'email' => 'E-mail',
+            'password' => 'Password',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'required' => 'the :attribute should not be empty, please add the :attribute!',
+            'max' => 'the :attribute is too long!',
+            'unique' => ':attribute already exists please try another one',
+            'between' => ':attribute should be between :min - :max',
+            'confirmed' => 'Password does not match please try again'
+        ];
+    }
+
+    /**
+     * if the validation failed it return a json response
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Failed Validate Data',
+            'errors' => $validator->errors(),
+        ], 422));
+    }
+}

24109b76532f6a3aa73c2dc564d7bb80c55ba094
BsHeR4 <bshermahayni@gmail.com>
2025-05-24 13:11:58 +0300

    Setup LocationController

---
+++ app/Http/Controllers/Api/LocationController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LocationRequest;
+use App\Http\Resources\LocationResource;
+use App\Services\Interfaces\LocationServiceInterface;
+use Illuminate\Http\Request;
+
+class LocationController extends Controller
+{
+
+    /**
+     * Service to handle location-related logic 
+     * and separating it from the controller
+     * 
+     * @var LocationServiceInterface
+     */
+    protected $locationService;
+
+    /**
+     * LocationController constructor
+     *
+     * @param LocationServiceInterface $locationService
+     */
+    public function __construct(LocationServiceInterface $locationService)
+    {
+        // Inject the LocationService to handle location-related logic
+        $this->locationService = $locationService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $locations = $this->locationService->getAll();
+        return $this->successResponse('success', LocationResource::collection($locations));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(LocationRequest $request)
+    {
+        $data = $request->validated();
+        $location = $this->locationService->store($data);
+        return $this->successResponse('success', new LocationResource($location));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $location = $this->locationService->get($id);
+        return $this->successResponse('success', new LocationResource($location));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(LocationRequest $request, string $id)
+    {
+        $data = $request->validated();
+        $location = $this->locationService->update($data, $id);
+        return $this->successResponse('success', new LocationResource($location));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $this->locationService->destroy($id);
+        return $this->successResponse('success');
+    }
+}
